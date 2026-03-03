5aeae5d620259cad232148e6bb6642015135d7b1
bb2226-pandu <bb2226@srmist.edu.in>
2026-03-03 22:02:23 +0530

    Added problem 5 of week 1 and week 2

---
+++ level2_pro2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class level2_pro2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter base in cm: ");
+        double base = sc.nextDouble();
+
+        System.out.print("Enter height in cm: ");
+        double height = sc.nextDouble();
+
+        double areaCm = 0.5 * base * height;
+        double areaInch = areaCm / (2.54 * 2.54);
+
+        System.out.println("Area in sq cm = " + areaCm);
+        System.out.println("Area in sq inches = " + areaInch);
+
+        sc.close();
+    }
+}
