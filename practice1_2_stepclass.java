bb8799f928b20a83716cec345e0e45dbdcef418e
bb2226-pandu <bb2226@srmist.edu.in>
2026-03-03 21:49:49 +0530

    Added Problem 2 of week 1 and week 2

--- practice1_2_stepclass.java
+++ practice1_2_stepclass.java
@@ -1,10 +1,10 @@
-public class Problem1{
+public class practice1_2_stepclass{
 	public static void main(String[] args){
 		 int birthYear = 2000;
 		 int currentYear = 2024;
 		 
 		 int age = currentYear - birthYear;
 		 
-		 System.ou.println("Harry's age in 2024 is"+age);
+		 System.out.println("Harry's age in 2024 is"+age);
 	}
 }
